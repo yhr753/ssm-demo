08f59358e947b35bd702559481eea8591ec72895
yhr753 <61617515+yhr753@users.noreply.github.com>
2021-05-07 18:37:01 +0800

    Add files via upload

---
+++ sample/src/main/java/com/yhr/sample/demo.java
@@ -0,0 +1,20 @@
+package com.yhr.sample;
+
+import static com.yhr.sample.SMSUtils.VALIDATE_CODE;
+
+/**
+ * @program: sample
+ * @description: 测试阿里短息服务
+ * @packagename: com.aliyun.sample.s
+ * @author: yanghongrong
+ * @date: 2021-05-07 17:57
+ **/
+public class demo {
+    public static void main(String[] args)throws Exception {
+        // 第一个参数  工具类里定义的短信模板的  模版CODE
+        // 第二个参数  要发送的手机号
+        // 第三个参数  短信模板里的 ${code}
+        String msg = SMSUtils.sendShortMessage(VALIDATE_CODE, "19873364674", "111111");
+        System.out.println(msg);
+    }
+}
