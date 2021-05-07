af248cde41a84a0d6a8edc4c1b58947037bbcdf4
yhr753 <61617515+yhr753@users.noreply.github.com>
2021-05-07 18:49:53 +0800

    Update SMSUtils.java

--- sample/src/main/java/com/yhr/sample/SMSUtils.java
+++ sample/src/main/java/com/yhr/sample/SMSUtils.java
@@ -13,8 +13,8 @@ import com.aliyuncs.profile.IClientProfile;
  * 短信发送工具类
  */
 public class SMSUtils {
-    public static final String VALIDATE_CODE = "SMS_215352971";//发送短信验证码  短信模板
-//    public static final String ORDER_NOTICE = "SMS_181865525";//体检预约成功通知
+    public static final String VALIDATE_CODE = "";//发送短信验证码  短信模板
+//    public static final String ORDER_NOTICE = "";//体检预约成功通知
     /**
      * 发送短信
      * @param phoneNumbers
@@ -29,8 +29,8 @@ public class SMSUtils {
         final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
         // 替换成你的AK
-        final  String accessKeyId = "LTAI5tRBEHUsTAfyhdjAFmsH";
-        final  String accessKeySecret = "crtUQpWorfBXpqcnt45NLiw7VMuHif";
+        final  String accessKeyId = "";
+        final  String accessKeySecret = "";
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
@@ -42,7 +42,7 @@ public class SMSUtils {
         // 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
         request.setPhoneNumbers(phoneNumbers);
         // 必填:短信签名-可在短信控制台中找到
-        request.setSignName("闪电租车");
+        request.setSignName("");
         // 必填:短信模板-可在短信控制台中找到
         request.setTemplateCode(templateCode);
         // 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
